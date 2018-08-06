9124e086bdd31006f093f1cbb585d807a30005d4
mitchr <tug72074@temple.edu>
2018-08-06 19:20:44 -0400

    Add rudimentary book class

---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+	def __init__(self, title, author, checkoutDate, returnDate):
+		self.title = title
+		self.author = author
+		self.checkoutDate = checkoutDate
+		self.returnDate = returnDate
+	
