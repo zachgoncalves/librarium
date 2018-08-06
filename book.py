680d3e3b3e427838b30a1731e5814bda56a65254
zachgoncalves <zach.j.goncalves@gmail.com>
2018-08-06 19:37:32 -0400

    Rename book.py to library.py

--- book.py
+++
@@ -1,6 +0,0 @@
-class Book:
-	def __init__(self, title, author, checkoutDate, returnDate):
-		self.title = title
-		self.author = author
-		self.checkoutDate = checkoutDate
-		self.returnDate = returnDate
---
+++ library.py
@@ -0,0 +1,6 @@
+class Book:
+	def __init__(self, title, author, checkoutDate, returnDate):
+		self.title = title
+		self.author = author
+		self.checkoutDate = checkoutDate
+		self.returnDate = returnDate
